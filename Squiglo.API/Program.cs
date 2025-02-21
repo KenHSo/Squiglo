55ddae440c564d40a357049627423d4bf7bb8841
Kenneth Haagen Sørensen <khso63309@edu.ucl.dk>
2025-02-21 12:14:19 +0100

    Removed SQLite dependency and deleted SQLite database squiglo.db

--- Squiglo.API/Program.cs
+++ Squiglo.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Configure EF Core with SQLite
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
